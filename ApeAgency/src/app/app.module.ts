05925a46fdd405a03c2b0b66084df675537167eb
Ankit Vashishta <ankitjobalerts@gmail.com>
2018-08-09 02:25:55 +0530

    Added modules like bootstrap and added components

--- ApeAgency/src/app/app.module.ts
+++ ApeAgency/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { PageComponent } from './page/page/page.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
